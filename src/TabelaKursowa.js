233dc81bc5a09b5c44ae75d67f7a7739f390232f
GryczonJ <jan.gryczon1@gmail.com>
2026-01-12 18:04:15 +0100

    Remove commented state description in TabelaKursowa component

--- src/TabelaKursowa.js
+++ src/TabelaKursowa.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const TabelaKursowa = () => {
-  // Stan przechowujący wybraną tabelę (domyślnie 'A')
   const [selectedTable, setSelectedTable] = useState('A');
   const [tableData, setTableData] = useState(null);
   const [loading, setLoading] = useState(true);
